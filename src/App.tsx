cafc968a6ef3e2b05c863c7bc2b05a477904917e
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-07 10:34:31 +0100

    random number state in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
+import { VariableWrapper } from "./components/VariableWrapper";
 
 function App() {
   const [firstname, setFirstName] = useState("Kim");
+  const [randomNumber, setRandomNumber] = useState(0);
   return (
     <>
       <h1 className="text-5xl text-red-500">Hello World</h1>
 
-      <div className="bg-slate-300 p-4 flex gap-2">
+      <VariableWrapper>
         <button onClick={() => setFirstName("Robert")}>Change</button>
         <p className="text-2xl">First name: {firstname}</p>
-      </div>
+      </VariableWrapper>
+
+      <VariableWrapper>
+        <button onClick={() => setRandomNumber(Math.random())}>Change</button>
+        <p>Random Number: {randomNumber}</p>
+      </VariableWrapper>
     </>
   );
 }
---
+++ src/components/VariableWrapper.tsx
@@ -0,0 +1,11 @@
+interface IProps {
+  children: React.ReactNode;
+}
+
+export const VariableWrapper = ({ children }: IProps) => {
+  return (
+    <>
+      <div className="bg-slate-300 p-4 flex gap-2">{children}</div>
+    </>
+  );
+};
