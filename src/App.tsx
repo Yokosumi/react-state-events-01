47983b155918081c19d959f8fbdca99441b34cae
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-07 10:24:59 +0100

    string state variable in

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [firstname, setFirstName] = useState("Kim");
   return (
     <>
       <h1 className="text-5xl text-red-500">Hello World</h1>
+
+      <div className="bg-slate-300 p-4 flex gap-2">
+        <button onClick={() => setFirstName("Robert")}>Change</button>
+        <p className="text-2xl">First name: {firstname}</p>
+      </div>
     </>
   );
 }
